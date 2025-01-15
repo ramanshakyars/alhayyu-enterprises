51eced08071c758539316d1d7b070fc0956fe696
ramanshakyars <ramanshakyars12@gmail.com>
2025-01-15 16:38:50 +0530

    landing page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [NavbarComponent,LandingPageComponent],
+  imports: [NavbarComponent,LandingPageComponent,FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
